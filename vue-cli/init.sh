89ee81a2052aad5dcb5efb5db8da9c364ffc4990
Torsten Goerke <torsten.goerke@tu-dresden.de>
2018-06-15 10:23:59 +0200

    Change: Node version, App port

--- vue-cli/init.sh
+++ vue-cli/init.sh
@@ -6,10 +6,26 @@ if [ -f "$FILE" ]; then
      npm install
      npm run dev
 else
-    echo "$DIR is Empty: vue-cli init"
-    cd /code
-    yes | vue init webpack .
-    npm install
-    npm run dev
+    echo "$DIR is Empty"
+    echo "Enter docker container with:"
+    echo "docker exec -ti vue-cli-docker bash"
+    #cd /code
+
+    #cd search-frontend
+    #npm install
+    #npm run dev
+
+    #cd axios-minimal-example
+    #npm install
+    #npm run dev
+
+    #cd bootstrap-vue-project
+    #npm install
+    #npm run dev
 fi
-# rest of the logic
+
+# while bash is idling you can connect to it and do stuff:
+# docker exec -ti vue-cli-docker bash
+while true; do :; sleep 2; done
+
+
